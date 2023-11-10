28a2663f7929f4e1635866cd5f7186a164913ada
Hoxfire <jorr8785@gmail.com>
2023-11-09 21:48:20 -0500

    working on turning the player, its almost there just needs some updating from version 3.4

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,9 +1,21 @@
 extends Node3D
 
+func _process(delta):
+	look_at(ScreenPointToRay(), Vector3.UP)
+	
+func ScreenPointToRay():
+	var spaceState = get_world_3d().direct_space_state
+	
+	var mousePos = get_viewport().get_mouse_position()
+	var camera = get_tree().root.get_camera_3d()
+	
+	var rayOrigin = camera.project_ray_origin(mousePos)
+	var rayEnd = rayOrigin + camera.project_ray_normal(mousePos) * 2000
+	var thing = PhysicsRayQueryParameters3D.create(rayOrigin,rayEnd)
+	var rayArray = spaceState.intersect_ray(thing)
+	if rayArray.has("position"):
+		return rayArray["position"]
+	return Vector3()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-var pos = 0.0
 
-var speed = 2
+
